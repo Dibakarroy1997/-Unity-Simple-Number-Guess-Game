a33be73aef547aa191a463f2a08d6554af9aadc6
BAKA <allinonedibakar@gmail.com>
2018-01-12 10:42:35 +0530

    Added Source file

---
+++ src/Assets/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	//Load a level
+	public void LoadLevel(string name){
+		Debug.Log ("Loading Level " + name);
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Quiting game.");
+		//UnityEditor.EditorApplication.isPlaying = false;
+		Application.Quit ();
+	}
+}
---
+++ src/Assets/NumberGuess.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberGuess : MonoBehaviour {
+	//Instance variables
+	int max = 1001, min = 1, guess, tries = 10;
+	public Text text;
+	
+	int NextGuess(){
+		if (tries <= 0) {
+			Application.LoadLevel("Win");
+		}
+
+		guess = Random.Range (min, max);
+
+		text.text = "Is the number " + guess 
+			+ "?. If no then is it higher or lower than " + guess 
+			+ "? By the way, I still have " + tries + " chances left.";
+
+		tries--;
+		return(guess);
+	}
+	
+	public void GuessHigher(){
+		min = guess;
+		NextGuess ();
+	}
+	
+	public void GuessLower(){
+		max = guess;
+		NextGuess ();
+	}
+
+	void Start(){
+		NextGuess ();
+	}
+}
